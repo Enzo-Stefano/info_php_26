9d17cedbdaccc531678bebe2827495fdb61aa6c4
suporte <suporte@DESKTOP-LQ0UB60.localdomain>
2026-06-02 20:08:05 -0300

    aula do copia e cola

--- Endereco.php
+++ Endereco.php
@@ -4,14 +4,81 @@ class Endereco {
     private $rua;
     private $numero;
     private $bairro;
+    private $cidade;
     private $estado;
     private $cep;
 
-    public function __construct($rua, $numero, $bairro, $estado, $cep) {
+    public function __construct($rua, $numero = "S/N", $bairro, $estado, $cep, $cidade) {
+        $this->estado = $estado;
+        $this->cidade = $cidade;
+        $this->cep = $cep;
+        $this->bairro = $bairro;
         $this->rua = $rua;
         $this->numero = $numero;
-        $this->bairro = $bairro;
-        $this->estado = $estado;
-        $this->cep = $cep;
     }
+
+    public function getRua() {
+        return $this->rua;
+    }
+
+    public function getNumero() {
+        return $this->numero;
+    }
+    
+    public function getBairro() {
+        return $this->bairro;
+    }
+    
+    public function getEstado() {
+        return $this->estado;
+    }
+    
+    public function getCep() {
+        return $this->cep;
+    }
+    
+    public function validarEndereco() {
+        return $this->validarEstado() && $this->validarCidade() && $this->validarCEP() && $this->validarBairro() && $this->validarRua();
+    }
+
+    
+    public function validarEstado() {
+        if (empty($this->estado)) {
+            return false;
+        }
+
+        return true;    
+    }
+
+    public function validarCidade() {
+        if (empty($this->cidade)) {
+            return false;
+        }
+
+        return true;    
+    }
+
+    public function validarCEP() {
+        if (empty($this->cep)) {
+            return false;
+        }
+
+        return true;    
+    }
+
+    public function validarBairro() {
+        if (empty($this->bairro)) {
+            return false;
+        }
+
+        return true;    
+    }
+
+    public function validarRua() {
+        if (empty($this->rua)) {
+            return false;
+        }
+
+        return true;
+    }    
 }
