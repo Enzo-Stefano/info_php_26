1983fb2d1e89db12f61c54df32ef0883f307b205
Enzo-Stefano <enzostefanop.massutti11@gmail.com>
2026-07-21 21:19:25 -0300

    versao profe

--- src/docs/README.md
+++ src/docs/README.md
@@ -1,111 +1,138 @@
 # info_php_26
 
-Básico:
-- open folder -> /var/www/html -> login github -> link rep "info_php_26" -> VScode -> git clone (https://github.com/Enzo-Stefano/info_php_26);
-- "localhost/" == "/var/www/html/" 
+# Tutorial
+- Abrir vscode ver se ta logado ubuntu (canto inferior esquerdo), depois file open folder: seleciona tudo e apaga.
 
-DIA 17/03/26:
-- m php, fechar a tag (?>) só se tiver outro tipo de código junto;
-- "==" compara o "valor" (ignora o tipo), "===" leva em com-paração o título;
-- operadores:
-    $resultado = 0;
-    $valor1 = 50;
-    $valor2 = 200;
+- /var/www/html e da um ok. Após, terminal novo terminal.
+git clone LINK_GITHUB da um enter. Novamente, file open folder: seleciona tudo e apaga.
 
-    $resultado = $resultado + $valor1; // 50;
-    $resultado += $valor1; // 50;
-    
-    $a = $a + 1;
-    $a +=1;
-    $a++;
-- operadores lógicos:
-    && // (and) também funciona
-    || // (or) também funciona
-    !  (1 != 2)
-    ==  (2==2)
-    <
-    >
-    <=
-    >=
-- Tipos de dados Primitivos
-    $numero = 10; // int || integer
-    $numero = 10.50; // float, long || Double (Numeric) Decimal
-    $numero = "10"; // String
-    $numero = '1';// char || character
+- /var/www/html/curso_php_26 e da um ok. Após, terminal novo terminal verifica se esta em: /var/www/html/curso_php_26.
 
-    // Tipos de dados de estrutura
-    // Array || vetor
-    $array = [];
-    $array = array();
-    $objeto = new stdClass(); // objeto ou classe
-    $objUsuario = new Usuario();
+# Configurar apache para aceitar rotas
+- Editar o arquivo
+```bash
+sudo vim /etc/apache2/sites-available/000-default.conf
+```
 
-git flow
-- criação da branch dev
+- Para editar pressionar a tecla insert
 
-linux:
-    sudo service mariadb start
-    sudo mysql -uroot -p
+- Adcionar o conteudo de directory(abaixo): <Directory "/var/www/html/">...</Directory>
 
-#mariadb
+```bash
+   <VirtualHost *:80>
+      ServerName localhost
+      ServerAlias localhost
+      DocumentRoot "/var/www/html"
+      <Directory "/var/www/html/">
+         Options +Indexes +Includes +FollowSymLinks +MultiViews
+         AllowOverride All
+         Require all granted
+      </Directory>
+   </VirtualHost>
+```
 
-CREATE database info_php_26;
+- Para sair do editor VIM pressionar `ESC` seguido de `:wq!`
+- Executar o comando:
+```bash
+   sudo a2enmod rewrite
+```
 
-CREATE USER 'aluno'@localhost IDENTIFIED BY '1234';
+- Parar e Iniciar o apache:
+   - ```bash
+         sudo service apache2 stop
+      ```
+   - ```bash
+         sudo service apache2 start
+      ```
 
-GRANT ALL PRIVILEGES ON *.* TO 'aluno'@localhost IDENTIFIED BY '1234';
 
-flush privileges;
+# MariaDB
 
-https://phoenixnap.com/kb/how-to-create-mariadb-user-grant-privileges#:~:text=To%20create%20a%20new%20MariaDB, to%20a%20local%20MySQL%20server.
+## Executar no terminal os comandos abaixo
+sudo service mariadb start
 
+sudo mysql -uroot -p
+
+```BASH
+   CREATE database info_php_26;
+
+   SHOW DATABASES;
+
+   CREATE USER 'aluno'@localhost IDENTIFIED BY '1234';
+
+   GRANT ALL PRIVILEGES ON *.* TO 'aluno'@localhost IDENTIFIED BY '1234';
+
+   flush privileges;
+```
+
+https://phoenixnap.com/kb/how-to-create-mariadb-user-grant-privileges#:~:text=To%20create%20a%20new%20MariaDB,
+to%20a%20local%20MySQL%20server.
 
 set session sql_mode = 'No_engine_substitution';
 
--- Retornar se o filtro for atendido select u.* from usuario as u inner join pessoa_fisica as pf ON u.id = pf.usuario_alteracao;
+```SQL
+-- Retornar se o filtro for atendido
+select u.* from usuario as u
+inner join pessoa_fisica as pf
+ON u.id = pf.usuario_alteracao;
 
--- Retornar tudo(usuario + Pessoa_fisica) se encontrar -- senao retorna os dados de usuario select u.* from usuario as u left join pessoa_fisica as pf ON u.id = pf.usuario_alteracao;
+-- Retornar tudo(usuario + Pessoa_fisica) se encontrar
+-- senao retorna os dados de usuario
+select u.* from usuario as u
+left join pessoa_fisica as pf
+ON u.id = pf.usuario_alteracao;
 
--- Retornar tudo(Pessoa_fisica + usuario) se encontrar -- senao retorna os dados da Pessoa_fisica select u.* from usuario as u right join pessoa_fisica as pf ON u.id = pf.usuario_alteracao;
 
--- Deixa a data e hora automatica no insert e update
-Consultas
-    SELECT p.nome, f.cargo, f.salario, p.idade 
-    FROM funcionario f
-    RIGHT JOIN Pessoa p ON f.idPessoa = p.id;
+-- Retornar tudo(Pessoa_fisica + usuario) se encontrar
+-- senao retorna os dados da Pessoa_fisica
+select u.* from usuario as u
+right join pessoa_fisica as pf
+ON u.id = pf.usuario_alteracao;
 
-    SELECT *
-    FROM funcionario f
-    RIGHT JOIN Pessoa p ON f.nome = p.nome;
 
-    SELECT * FROM funcionario f;
-Adição de campo de controle
-    ALTER TABLE info_php_26.estados MODIFY COLUMN criadoEm TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-    ALTER TABLE info_php_26.estados MODIFY COLUMN atualizadoEm TIMESTAMP DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP;
 
-    ALTER TABLE info_php_26.cidades MODIFY COLUMN criadoEm TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-    ALTER TABLE info_php_26.cidades MODIFY COLUMN atualizadoEm TIMESTAMP DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP;
+-- TODOS ENDERECOS USADOS EM PESSOAS
+SELECT p.id as "idPessoa", p.nome as "nomePessoa", p.cpf, ende.estado, ende.cidade, ende.cep, ende.bairro, ende.rua, ende.numero
+FROM endereco ende
+JOIN pessoa p ON ende.id = p.idEndereco;
 
-    ALTER TABLE info_php_26.endereco MODIFY COLUMN criadoEm TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-    ALTER TABLE info_php_26.endereco MODIFY COLUMN atualizadoEm TIMESTAMP DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP;
+SELECT p.id as "idPessoa", p.nome as "nomePessoa", p.cpf, ende.estado, ende.cidade, ende.cep, ende.bairro, ende.rua, ende.numero
+FROM endereco ende
+INNER JOIN pessoa p ON ende.id = p.idEndereco
+WHERE UPPER(ende.cidade) like "%BG%";
 
-    ALTER TABLE info_php_26.funcionario MODIFY COLUMN criadoEm TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-    ALTER TABLE info_php_26.funcionario MODIFY COLUMN atualizadoEm TIMESTAMP DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP;
+SELECT p.id as "idPessoa", p.nome as "nomePessoa", p.cpf, ende.estado, ende.cidade, ende.cep, ende.bairro, ende.rua, ende.numero
+FROM endereco ende
+left JOIN pessoa p ON ende.id = p.idEndereco;
 
-    ALTER TABLE info_php_26.pessoa MODIFY COLUMN criadoEm TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
-    ALTER TABLE info_php_26.pessoa MODIFY COLUMN atualizadoEm TIMESTAMP DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP;
 
-MVC:
-    Model = banco de dados;
-    View = interface do usuário;
-    Controller = validar dados, regras de negócio, criar OU atualizar;
+SELECT p.id as "idPessoa", p.nome as "nomePessoa", p.cpf, ende.estado, ende.cidade, ende.cep, ende.bairro, ende.rua, ende.numero
+FROM endereco ende
+right JOIN pessoa p ON ende.id = p.idEndereco;
 
-DDD (segue as regra da empresa):
-    Domain
-    Driven
-    Development
+SELECT p.id as "idPessoa", p.nome as "nomePessoa", p.cpf, func.cargo, func.cracha, func.salario
+FROM funcionario func
+INNER JOIN pessoa p ON func.idPessoa = p.id;
 
-Comm Navegador X back:
-    $_GET // (oq o navegador faz)(sempre busca)id, nome ...
-    $_POST // (Javascript)(Busca mas foco em envio) nome, sobrenome
-    $_REQUEST // (requisições) *
+-- Adicionar campos e deixar a data e hora automatica no insert e update
+ALTER TABLE info_php_26.funcionario ADD criadoEm TIMESTAMP NULL;
+ALTER TABLE info_php_26.funcionario ADD atualizadoEm TIMESTAMP NULL;
+
+ALTER TABLE info_php_26.funcionario MODIFY COLUMN criadoEm TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE info_php_26.funcionario MODIFY COLUMN atualizadoEm TIMESTAMP DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP;
+
+-- OU tudo em um unico SQL
+ALTER TABLE info_php_26.cidades ADD criadoEm TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE info_php_26.cidades ADD atualizadoEm TIMESTAMP DEFAULT NULL ON UPDATE CURRENT_TIMESTAMP;
+
+
+-- Campos comuns de todas as tabelas:
+   -- id(pk)(tinyint), criadoEm(TIMESTAMP/DATETIME), atualizadoEm(TIMESTAMP/DATETIME)
+-- Criar tabela de estados
+-- Criar tabela de cidades
+  -- DICA: Site do IBGE tem as informações.
+-- Criar FKs dessas tabelas em endereco(tabela) com alter table
+   -- alterar os campos para idEstado e idCidade
+-- Criar tabela de usuarios
+-- Criar FK de pessoa na tabela usuario
+```
--- src/docs/database/dump-info_php_26-202606232201.sql
+++ src/docs/database/dump-info_php_26-202606232201.sql
@@ -24,15 +24,15 @@ DROP TABLE IF EXISTS `cidades`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `cidades` (
   `id` tinyint(4) NOT NULL AUTO_INCREMENT,
-  `nome` varchar(100) NOT NULL,
-  `populacao` decimal(10,0) NOT NULL,
+  `nome` varchar(255) NOT NULL,
+  `populacao` float DEFAULT NULL,
+  `idEstado` tinyint(4) DEFAULT NULL,
   `criadoEm` timestamp NOT NULL DEFAULT current_timestamp(),
   `atualizadoEm` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
-  `idEstado` tinyint(4) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `cidades_estados_FK` (`idEstado`),
-  CONSTRAINT `cidades_estados_FK` FOREIGN KEY (`idEstado`) REFERENCES `estados` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  CONSTRAINT `cidades_estados_FK` FOREIGN KEY (`idEstado`) REFERENCES `estados` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -41,7 +41,7 @@ CREATE TABLE `cidades` (
 
 LOCK TABLES `cidades` WRITE;
 /*!40000 ALTER TABLE `cidades` DISABLE KEYS */;
-INSERT INTO `cidades` VALUES (1,'Bento Gonçalves',100,'2026-06-23 22:59:58','0000-00-00 00:00:00',1),(2,'Caxias do Sul',101,'2026-06-23 23:00:31','0000-00-00 00:00:00',1),(3,'Rio de Janeiro',102,'2026-06-23 23:01:05','0000-00-00 00:00:00',2);
+INSERT INTO `cidades` VALUES (1,'Bento Gonçalves',140000,1,'2026-06-23 22:58:48','2026-06-23 22:59:14'),(2,'Florianopolis',600325,2,'2026-06-23 23:00:56','0000-00-00 00:00:00'),(3,'Balneario Camboriu',423546,2,'2026-06-23 23:01:19','0000-00-00 00:00:00'),(4,'Londrina',235678,3,'2026-06-23 23:01:49','0000-00-00 00:00:00'),(5,'Pato Branco',122320,3,'2026-06-23 23:02:16','0000-00-00 00:00:00'),(6,'São Paulo',2068580,4,'2026-06-23 23:02:41','0000-00-00 00:00:00'),(7,'Campinas',750265,4,'2026-06-23 23:03:00','2026-06-23 23:03:08');
 /*!40000 ALTER TABLE `cidades` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -54,6 +54,8 @@ DROP TABLE IF EXISTS `endereco`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `endereco` (
   `id` tinyint(4) NOT NULL AUTO_INCREMENT,
+  `estado` varchar(2) NOT NULL,
+  `cidade` varchar(255) NOT NULL,
   `cep` varchar(10) NOT NULL,
   `bairro` varchar(255) NOT NULL,
   `rua` varchar(512) NOT NULL,
@@ -65,9 +67,9 @@ CREATE TABLE `endereco` (
   PRIMARY KEY (`id`),
   KEY `endereco_estados_FK` (`idEstado`),
   KEY `endereco_cidades_FK` (`idCidade`),
-  CONSTRAINT `endereco_cidades_FK` FOREIGN KEY (`idCidade`) REFERENCES `cidades` (`id`) ON UPDATE CASCADE,
-  CONSTRAINT `endereco_estados_FK` FOREIGN KEY (`idEstado`) REFERENCES `estados` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  CONSTRAINT `endereco_cidades_FK` FOREIGN KEY (`idCidade`) REFERENCES `cidades` (`id`),
+  CONSTRAINT `endereco_estados_FK` FOREIGN KEY (`idEstado`) REFERENCES `estados` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -76,7 +78,7 @@ CREATE TABLE `endereco` (
 
 LOCK TABLES `endereco` WRITE;
 /*!40000 ALTER TABLE `endereco` DISABLE KEYS */;
-INSERT INTO `endereco` VALUES (1,'95700000','Centro','General Osório','444','2026-06-17 00:08:43','2026-06-23 23:02:48',1,2),(2,'95707000','Universitário','Principal','102','2026-06-17 00:08:43','2026-06-23 23:02:48',1,2),(3,'80500500','Ipanema','Da Silva','202','2026-06-17 00:08:43','2026-06-23 23:02:48',2,3),(4,'95700500','Botafogo','Natal','421','2026-06-17 00:08:43','2026-06-23 23:02:48',1,1),(5,'95707000','Borgo','São Paulo','','2026-06-17 00:08:43','2026-06-23 23:02:49',1,1);
+INSERT INTO `endereco` VALUES (1,'RS','BG','95707000','Centro','das andradas','123','2026-06-17 00:43:50','2026-06-17 00:43:53',NULL,NULL),(5,'SC','Floripa','95706000','Centro','Dom Pedro II','456','2026-06-17 00:43:50','2026-06-17 00:43:53',NULL,NULL),(6,'SC','BC','94850330','Cohab','Marechal Peixoto','852','2026-06-17 00:43:50','2026-06-17 00:43:53',NULL,NULL),(7,'SC','Floripa','95706000','Licorsul','Dom Pedro II','74','2026-06-17 00:43:50','2026-06-17 00:43:53',NULL,NULL),(8,'RS','BG','95707000','Maria Goretti','Pedro Ernesto','S/N','2026-06-17 00:43:50','2026-06-17 00:43:53',NULL,NULL),(9,'RS','BG','95707000','Borgo','Sao Paulo','951','2026-06-17 00:43:50','2026-06-17 00:43:53',NULL,NULL),(10,'PR','Londrina','85707012','Centro','13 De Maio','10','2026-06-17 00:43:50','2026-06-17 00:43:53',NULL,NULL),(11,'PR','Pato Branco','85707000','Centro','Dom Pedro II','741','2026-06-17 00:43:50','2026-06-17 00:43:53',NULL,NULL),(12,'SC','BC','94850330','Atlantica','1580','3','2026-06-17 00:43:50','2026-06-23 23:19:31',NULL,NULL);
 /*!40000 ALTER TABLE `endereco` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -91,12 +93,10 @@ CREATE TABLE `estados` (
   `id` tinyint(4) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
   `sigla` varchar(2) NOT NULL,
-  `populacao` decimal(10,0) NOT NULL,
-  `numeroMunicipios` decimal(10,0) NOT NULL,
   `criadoEm` timestamp NOT NULL DEFAULT current_timestamp(),
   `atualizadoEm` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -105,7 +105,7 @@ CREATE TABLE `estados` (
 
 LOCK TABLES `estados` WRITE;
 /*!40000 ALTER TABLE `estados` DISABLE KEYS */;
-INSERT INTO `estados` VALUES (1,'Rio Gande do Sul','RS',200,2,'2026-06-17 00:51:21','2026-06-17 00:51:31'),(2,'Rio de Janeiro','RJ',201,1,'2026-06-23 22:59:09','0000-00-00 00:00:00');
+INSERT INTO `estados` VALUES (1,'Rio Grande do Sul','RS','2026-06-17 00:31:17','2026-06-17 00:35:08'),(2,'Santa Catarina','SC','2026-06-17 00:33:25','2026-06-17 00:35:08'),(3,'Parana','PR','2026-06-17 00:35:36','2026-06-17 00:36:01'),(4,'São Paulo','SP','2026-06-17 00:39:47','2026-06-17 00:42:08');
 /*!40000 ALTER TABLE `estados` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -128,9 +128,9 @@ CREATE TABLE `funcionario` (
   `criadoEm` timestamp NOT NULL DEFAULT current_timestamp(),
   `atualizadoEm` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
-  KEY `funcionario_Pessoa_FK` (`idPessoa`),
-  CONSTRAINT `funcionario_Pessoa_FK` FOREIGN KEY (`idPessoa`) REFERENCES `pessoa` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  KEY `funcionario_pessoa_FK` (`idPessoa`),
+  CONSTRAINT `funcionario_pessoa_FK` FOREIGN KEY (`idPessoa`) REFERENCES `pessoa` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -139,7 +139,7 @@ CREATE TABLE `funcionario` (
 
 LOCK TABLES `funcionario` WRITE;
 /*!40000 ALTER TABLE `funcionario` DISABLE KEYS */;
-INSERT INTO `funcionario` VALUES (1,'Enzo','Massutti',2000,'Técnico','TI','2561532221',5,'2026-06-17 00:07:54','0000-00-00 00:00:00'),(2,'Seu','Jorge',100000,'Cantor','Rádio','2222222222',4,'2026-06-17 00:07:54','0000-00-00 00:00:00');
+INSERT INTO `funcionario` VALUES (2,'Ariel','Felippi',1223456,'Professor','Desenvolvimento','4501',3,'2026-06-23 22:29:13','2026-06-23 22:29:13'),(3,'Maria','Silva',456789123,'Secretaria','Atendimento','0052',8,'2026-06-23 22:29:13','2026-06-23 22:29:13');
 /*!40000 ALTER TABLE `funcionario` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -154,7 +154,7 @@ CREATE TABLE `pessoa` (
   `id` tinyint(4) NOT NULL AUTO_INCREMENT,
   `nome` varchar(512) NOT NULL,
   `idade` int(11) NOT NULL,
-  `telefone` varchar(20) NOT NULL,
+  `telefone` varchar(20) DEFAULT NULL,
   `cpf` varchar(14) DEFAULT NULL,
   `cnpj` varchar(18) DEFAULT NULL,
   `tipoPessoa` enum('F','J') NOT NULL DEFAULT 'F',
@@ -162,9 +162,9 @@ CREATE TABLE `pessoa` (
   `criadoEm` timestamp NOT NULL DEFAULT current_timestamp(),
   `atualizadoEm` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00' ON UPDATE current_timestamp(),
   PRIMARY KEY (`id`),
-  KEY `Pessoa_endereco_FK` (`idEndereco`),
-  CONSTRAINT `Pessoa_endereco_FK` FOREIGN KEY (`idEndereco`) REFERENCES `endereco` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  KEY `pessoa_endereco_FK` (`idEndereco`),
+  CONSTRAINT `pessoa_endereco_FK` FOREIGN KEY (`idEndereco`) REFERENCES `endereco` (`id`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -173,7 +173,7 @@ CREATE TABLE `pessoa` (
 
 LOCK TABLES `pessoa` WRITE;
 /*!40000 ALTER TABLE `pessoa` DISABLE KEYS */;
-INSERT INTO `pessoa` VALUES (1,'João',20,'54999990000','000.000.000-00',NULL,'F',1,'2026-06-17 00:09:21','0000-00-00 00:00:00'),(2,'Zegla',85,'5434555555',NULL,'00000000000','J',5,'2026-06-17 00:09:21','0000-00-00 00:00:00'),(3,'Maria',18,'54999880000','000.000.000-00',NULL,'F',1,'2026-06-17 00:09:21','0000-00-00 00:00:00'),(4,'Seu Jorge',60,'51944445555','000.000.000-00',NULL,'F',3,'2026-06-17 00:09:21','0000-00-00 00:00:00'),(5,'Enzo',21,'54996676980','000.000.000-00',NULL,'F',4,'2026-06-17 00:09:21','0000-00-00 00:00:00');
+INSERT INTO `pessoa` VALUES (3,'Ariel',33,'30552022','01234567891',NULL,'F',8,'2026-06-23 22:26:21','2026-06-23 22:26:21'),(4,'Enzo',18,'549995933','19876543210',NULL,'F',5,'2026-06-23 22:26:21','2026-06-23 22:26:21'),(5,'Rafaella',25,'54999915742','15975398741',NULL,'F',8,'2026-06-23 22:26:21','2026-06-23 22:26:21'),(6,'Lucas',22,'55981235879','00012458534',NULL,'F',12,'2026-06-23 22:26:21','2026-06-23 22:26:21'),(7,'Guilherme',30,'51987563247','55234794219','8526547886000185','J',12,'2026-06-23 22:26:21','2026-06-23 22:26:21'),(8,'Maria',22,'54982658434','52348698712','8526547892000175','J',9,'2026-06-23 22:26:21','2026-06-23 22:26:21'),(9,'Ana',22,'54982658434','52348698712','8526547892000175','J',NULL,'2026-06-23 22:26:21','2026-06-23 22:26:21');
 /*!40000 ALTER TABLE `pessoa` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -190,4 +190,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-06-23 22:01:37
+-- Dump completed on 2026-06-23 22:01:05
